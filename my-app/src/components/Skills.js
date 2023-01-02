4661cc2577e99756ebae048afb7b0ddd92f7daf2
Shrinivas Venkatesan <shrinivas160703@gmail.com>
2023-01-02 10:39:35 +0530

    fixed skills section

--- my-app/src/components/Skills.js
+++ my-app/src/components/Skills.js
@@ -57,42 +57,42 @@ function Skills() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(22,163,74)" class="w-7 h-7 lg:w-9 lg:h-9 inline">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                     </svg>
-                    <a href="R" className="ml-3 lg:text-xl">
+                    <span className="ml-3 lg:text-xl">
                         R
-                    </a>
+                    </span>
                     </div>
                     <div className="rounded-lg bg-gray-600 px-28 py-5 shadow-2xl text-gray-300 hover:text-white items-center mb-5 md:mb-0" id="hover">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(22,163,74)" class="w-7 h-7 lg:w-9 lg:h-9 inline">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                     </svg>
-                    <a href="statistics" className="ml-3 lg:text-xl">
+                    <span className="ml-3 lg:text-xl">
                         Statistics
-                    </a>
+                    </span>
                     </div>
                     <div className="rounded-lg bg-gray-600 px-28 py-5 shadow-2xl text-gray-300 hover:text-white items-center mb-5 md:mb-0" id="hover">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(22,163,74)" class="w-7 h-7 lg:w-9 lg:h-9 inline">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                     </svg>
-                    <a href="Data Analysis" className="ml-3 lg:text-xl">
+                    <span className="ml-3 lg:text-xl">
                         Data Analysis
-                    </a>
+                    </span>
                     </div>
                     <div className="rounded-lg bg-gray-600 px-28 py-5 shadow-2xl text-gray-300 hover:text-white items-center mb-5 md:mb-0 hidden lg:block" id="hover">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(22,163,74)" class="w-7 h-7 lg:w-9 lg:h-9 inline">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                     </svg>
-                    <a href="agile methods" className="ml-3 lg:text-xl">
+                    <span className="ml-3 lg:text-xl">
                         Agile Methods
-                    </a>
+                    </span>
                     </div>
                 </div>
                 <div className="rounded-lg bg-gray-600 px-28 py-5 shadow-2xl text-gray-300 hover:text-white items-center md:mt-10 mb-5 md:mb-0 block lg:hidden" id="hover">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(22,163,74)" class="w-7 h-7 lg:w-9 lg:h-9 inline">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                     </svg>
-                    <a href="agile methods" className="ml-3 text-md lg:text-xl">
+                    <span className="ml-3 text-md lg:text-xl">
                         Agile Methods
-                    </a>
+                    </span>
                 </div>
             </div>
         </section>
